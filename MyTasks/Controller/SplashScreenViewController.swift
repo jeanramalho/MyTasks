a5353b78cd0d787ae2a9352f3740e5f1e09645e1
jeanramalho.dev <jeanramalho.dev@gmail.com>
2025-03-20 10:42:29 -0300

    [add] - configura projeto para viewCode

---
+++ MyTasks/Controller/SplashScreenViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SplashScreenViewController.swift
+//  MyTasks
+//
+//  Created by Jean Ramalho on 20/03/25.
+//
+import Foundation
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+    
+    let contentView: SplashScreenView = SplashScreenView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+    
+    private func setup(){
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        view.addSubview(contentView)
+    }
+    
+    private func setConstraints(){
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: view.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
---
+++ MyTasks/View/SplashScreenView.swift
@@ -0,0 +1,36 @@
+//
+//  SplashScreenView.swift
+//  MyTasks
+//
+//  Created by Jean Ramalho on 20/03/25.
+//
+import Foundation
+import UIKit
+
+class SplashScreenView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI(){
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy(){
+        
+    }
+    
+    private func setConstraints(){
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+}
